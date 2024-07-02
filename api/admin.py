ab3a3769b0f69139e944ad92ea92759958b8074a
IldarSaygafarov2 <sayildar17@gmail.com>
2024-07-02 11:02:59 +0500

    added models to admin

--- api/admin.py
+++ api/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 from . import models
 
 
+class PhotoItemInline(admin.TabularInline):
+    model = models.PhotoItem
+    extra = 1
+
+@admin.register(models.Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    inlines = [PhotoItemInline]
+
+
 admin.site.register(models.PDFFile)
+admin.site.register(models.Photo, PhotoAdmin)
